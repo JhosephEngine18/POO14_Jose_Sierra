738dc46e769925ca06121f9bad3e053976eaeb84
DevSierra <josesielo1805@gmail.com>
2025-03-18 09:06:56 -0600

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+/*
+Universidad Amerike
+Author: José Sierra
+Pratice#:14
+Date: 18/03/25
+Description: This tells you what is your average about 7 numbers
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Numbers[7];
+void list(int numbers[], int size)
+{
+    cin >> Numbers[size];
+    numbers[size];
+}
+
+float Sum(int SumNum[], float dividedby, int R)
+{
+    int counter = 0;
+    for (int i = 0; i < 7; i++)
+    {  
+        R = R + SumNum[i];
+    }
+    R = R / dividedby;
+    return R;
+}
+
+int main()
+{   
+    
+    cout << "Type 7 numbers" <<endl;
+    for (int i = 0; i < 7; i++)
+    {
+        list(Numbers, i);
+    }
+
+    cout << "Your average is: " << Sum(Numbers, 7, 0) <<endl;
+    
+}
